fe9d772ce88a57f637964cdeaadec14f09ba9387
Sagnik Ghosh <sagnikghosh@Sagniks-MacBook-Air.local>
2024-04-21 11:47:27 +0530

    fixed margin

--- src/app/GridOfResizableDivs/GridOfResizableDivs.tsx
+++ src/app/GridOfResizableDivs/GridOfResizableDivs.tsx
@@ -2,51 +2,71 @@ import React, { useEffect, useState } from "react";
 import ResizableDiv from "../../components/ResizableDiv.tsx";
 
 const GridOfResizableDivs = ({ rows, columns }) => {
-  const [size, setSIze] = useState([window.innerWidth, window.innerHeight]);
-  
+  const [size, setSIze] = useState([
+    window.innerWidth - 50,
+    window.innerHeight,
+  ]);
+
   // Initialize a grid state to manage the size of each cell
   const initialSizes = Array.from({ length: rows }, () =>
-    Array.from({ length: columns }, () => ({ width: size[0] / columns, height: 200 }))
+    Array.from({ length: columns }, () => ({
+      width: size[0] / columns,
+      height: 200,
+    }))
   );
   const [gridSizes, setGridSizes] = useState(initialSizes);
 
-  const updateBlockSize = (rowIndex, colIndex, newSize) => {
-    const newSizes = gridSizes.map((row, rIndex) =>
-      row.map((cell, cIndex) => {
+  const updateBlockSize = (rowIndex, colIndex, newSize, dX, dY) => {
+    const newSizes = gridSizes.map((row, rIndex) => {
+      return row.map((cell, cIndex) => {
         if (rIndex === rowIndex && cIndex === colIndex) {
           return { ...cell, ...newSize };
         }
         return cell;
-      })
-    );
+      });
+    });
     setGridSizes(newSizes);
   };
 
   useEffect(() => {
     function updateWindowSize() {
-      setSIze([window.innerWidth, window.innerHeight]);
+      setSIze([window.innerWidth - 50, window.innerHeight]);
+      setGridSizes(
+        Array.from({ length: rows }, () =>
+          Array.from({ length: columns }, () => ({
+            width: (window.innerWidth - 50) / columns,
+            height: 200,
+          }))
+        )
+      );
     }
-    window.addEventListener('resize', updateWindowSize);
+
+    window.addEventListener("resize", updateWindowSize);
     updateWindowSize();
-    return () => window.removeEventListener('resize', updateWindowSize);
-  }, []);
+    return () => window.removeEventListener("resize", updateWindowSize);
+  }, [window.innerWidth]);
 
   return (
-    <div
-      style={{
-        display: "grid",
-        gridTemplateColumns: `repeat(${columns}, auto)`,
-      }}
-    >
-      {gridSizes.map((row, rowIndex) =>
-        row.map((size, colIndex) => (
-          <ResizableDiv
-            key={`${rowIndex}-${colIndex}`}
-            size={size}
-            onResize={(newSize) => updateBlockSize(rowIndex, colIndex, newSize)}
-          />
-        ))
-      )}
+    <div style={{ marginTop: "10px" }}>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: `repeat(${columns}, auto)`,
+          justifyContent: "center",
+        }}
+      >
+        {gridSizes.map((row, rowIndex) =>
+          row.map((size, colIndex) => (
+            <ResizableDiv
+              key={`${rowIndex}-${colIndex}`}
+              size={size}
+              onResize={(newSize, dX, dY) =>
+                updateBlockSize(rowIndex, colIndex, newSize, dX, dY)
+              }
+            />
+          ))
+        )}
+      </div>
     </div>
   );
 };
